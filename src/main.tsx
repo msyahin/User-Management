a51cfe5fe333c04eaacac7839493b9329fe87f7d
msyahin <m17syahin@gmail.com>
2025-11-12 14:34:39 +0800

    feat: theme mode

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { ModalManagerProvider } from './components/modal/use-modal-manager.tsx'
 import { modals } from './components/modal/registry.ts'
 import { ModalProvider } from './components/modal/use-modal.tsx'
 import { Toaster } from './components/ui/sonner.tsx'
+import { ThemeProvider } from './components/theme/theme-provider.tsx'
 
 const queryClient = new QueryClient()
 
@@ -15,8 +16,10 @@ createRoot(document.getElementById('root')!).render(
     <QueryClientProvider client={queryClient}>
       <ModalProvider>
         <ModalManagerProvider modals={modals}>
+          <ThemeProvider defaultTheme="system" storageKey="user-management-theme">
             <App />
-          <Toaster richColors position="top-right"/>
+            <Toaster richColors position="top-right" />
+          </ThemeProvider>
         </ModalManagerProvider>
       </ModalProvider>
     </QueryClientProvider>
--- src/pages/user-management/index.tsx
+++ src/pages/user-management/index.tsx
@@ -1,9 +1,13 @@
+import { ModeToggle } from "@/components/theme/theme-toggle";
 import UserManagementTable from "@/features/user-management/table";
 
 const UsersPage = () => {
 
   return (
     <div className="container mx-auto py-10">
+      <div className="flex justify-end mb-4">
+        <ModeToggle />
+      </div>
       <h1 className="text-2xl font-bold mb-4">User Management</h1>
       <UserManagementTable />
     </div>
